cdf46c33a452305bc85f4ea4f3958357d7625c14
Alberto Berroteran <aberroteran@live.com>
2014-04-10 13:51:32 -0430

    fix autoload

--- autoload.php
+++ autoload.php
@@ -1,7 +1,7 @@
 <?php
 function register_gettext_autoload(){
     spl_autoload_register(function($name){
-        $file = __DIR__ .'/src/'. str_replace('\\', DIRECTORY_SEPARATOR, $name) . '.php';
+        $file = __DIR__ .'/lib/'. str_replace('\\', DIRECTORY_SEPARATOR, $name) . '.php';
         if(is_readable($file))
             require $file;
     });
