4112f3b02db7611ab5f9a59c3032f57666c83d6d
Alberto Berroteran <aberroteran@live.com>
2014-04-10 12:32:23 -0430

    Implement autoload

---
+++ autoload.php
@@ -0,0 +1,8 @@
+<?php
+function register_gettext_autoload(){
+    spl_autoload_register(function($name){
+        $file = __DIR__ .'/lib/'. str_replace('\\', DIRECTORY_SEPARATOR, $name) . '.php';
+        if(is_readable($file))
+            require $file;
+    });
+}
